9275bbf8922cad2cc494779ebbc7c178f202b380
nogueira-fe <felipe_nogueira07@hotmail.com>
2023-10-08 18:45:48 -0300

    Requirements

--- terraespectral_ml_api/terraespectral_ml_api/settings.py
+++ terraespectral_ml_api/terraespectral_ml_api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -112,6 +112,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Static files (CSS, JavaScript, Images)
