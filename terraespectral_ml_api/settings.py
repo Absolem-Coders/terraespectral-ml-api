69d48fc4182f0e621331e9f2e6fb3a458cfb379a
nogueira-fe <felipe_nogueira07@hotmail.com>
2023-10-08 23:24:28 -0300

    cors fixing 7

--- terraespectral_ml_api/settings.py
+++ terraespectral_ml_api/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'terraespectral_ml_api.urls'
@@ -118,8 +118,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 CORS_ALLOWED_ORIGINS = [
-    'https://spectral.earth',
+    '*',
+    'https://terraespectral-web.up.railway.app',
+    "http://localhost:8000",
 ]
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_HEADERS = '*'
@@ -128,3 +132,23 @@ ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://terraespectral-ml-api.up.railway.app']
 CSRF_COOKIE_SECURE = False
 CSRF_COOKIE_SAMESITE = None
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_METHODS = [
+'DELETE',
+'GET',
+'OPTIONS',
+'PATCH',
+'POST',
+'PUT',
+]
+CORS_ALLOW_HEADERS = [
+'accept',
+'accept-encoding',
+'authorization',
+'content-type',
+'dnt',
+'origin',
+'user-agent',
+'x-csrftoken',
+'x-requested-with',
+]
